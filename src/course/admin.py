2363f7b653964712827c47c115db6ec590804f6c
= <xaralaxarala@gmail.com>
2019-09-10 13:50:04 +0000

    Populate admin

--- src/course/admin.py
+++ src/course/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Course, Chapter, Lesson
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Chapter)
+admin.site.register(Lesson)
