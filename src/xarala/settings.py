36f546f5a2d60043bf58df92a2979c68b97c8be6
xarala221 <xaralaxarala@gmail.com>
2019-09-29 09:41:59 -0400

    change static url

--- src/xarala/settings.py
+++ src/xarala/settings.py
@@ -145,8 +145,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = "/home/xarala/pyapps/xarala_ssr"
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,  'xarala/static')]
-
+#STATICFILES_DIRS = [os.path.join(BASE_DIR,  'xarala/static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 AUTH_USER_MODEL = 'users.CustomUser'
