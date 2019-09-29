c761d38bb079094a322e9f309dd554049ee68d9a
= <xaralaxarala@gmail.com>
2019-09-29 13:11:47 +0000

    Add staticfiles

--- src/xarala/settings.py
+++ src/xarala/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = "/home/xarala/pyapps/xarala_ssr"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,  'xarala/static')]
--- src/xarala/urls.py
+++ src/xarala/urls.py
@@ -14,5 +14,9 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
