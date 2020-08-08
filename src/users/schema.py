46a8ab27cac23094ed6a712bbf1c477a86d43901
Ousseynou Diop <xaralaxarala@gmail.com>
2020-08-08 20:31:45 +0000

    update user mutation

--- src/users/schema.py
+++ src/users/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from graphql import GraphQLError
 from graphene_django import DjangoObjectType
 from graphql_auth import mutations
 from graphql_auth.schema import MeQuery, UserQuery
@@ -10,6 +11,29 @@ from .models import CustomUser as User
 class UserType(DjangoObjectType):
     class Meta:
         model = User
+
+
+class UpdateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        userId = graphene.Int(required=True)
+        firstName = graphene.String()
+        lastName = graphene.String()
+        phone = graphene.String()
+        address = graphene.String()
+
+    def mutate(self, info, userId, firstName, lastName, phone, address):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError("Log in to edit user account!")
+        user = User.objects.get(id=userId)
+        user.first_name = firstName
+        user.last_name = lastName
+        user.address = address
+        user.phone = phone
+        user.save()
+        return UpdateUser(user=user)
 
 
 class AuthMutation(graphene.ObjectType):
@@ -36,7 +60,7 @@ class Query(UserQuery, MeQuery, graphene.ObjectType):
 
 
 class Mutation(AuthMutation, graphene.ObjectType):
-    pass
+    update_user = UpdateUser.Field()
 
 
 schema = graphene.Schema(query=Query)
