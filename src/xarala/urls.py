8ba28e69aba9dfceb262b16e998c140ae412b53d
Xarala <xaralaxarala@gmail.com>
2021-02-08 08:12:54 +0000

    Secure api

--- src/xarala/urls.py
+++ src/xarala/urls.py
@@ -1,19 +1,22 @@
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
-from django.views.decorators.csrf import csrf_exempt
+from django.urls import path, include
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     path("x-yarou/", admin.site.urls),
-    path("graphql/", csrf_exempt(GraphQLView.as_view(settings.DEBUG))),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=settings.DEBUG))),
     path("", include("pages.urls", namespace="pages")),
+    # path('accounts/', include('allauth.urls')),
     path("", include("course.urls", namespace="course")),
     path("podcasts/", include("podcast.urls", namespace="podcast")),
     path("users/", include("users.urls")),
     path("blog/", include("blog.urls", namespace="blog")),
     path("dashboard/", include("dashboard.urls", namespace="dashboard")),
+    # path("quiz/", include("quiz.urls", namespace="quiz")),
     path("search/", include("search.urls", namespace="search")),
     path("summernote/", include("django_summernote.urls")),
 ]
