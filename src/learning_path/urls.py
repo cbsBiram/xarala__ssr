6888bbf6e79a50ce5afc3009b3a22bcd4dfae8cd
ahmeth amar <ahmeth.amar@gmail.com>
2021-03-05 19:52:14 +0000

    add learning_path

--- src/learning_path/urls.py
+++ src/learning_path/urls.py
@@ -5,4 +5,9 @@ app_name = "learning_path"
 
 urlpatterns = [
     path("learning-path/", views.LearningPathView.as_view(), name="paths"),
+    path(
+        "learning-path/show/<str:title>/",
+        views.ShowLearningPathView.as_view(),
+        name="show_path",
+    ),
 ]
