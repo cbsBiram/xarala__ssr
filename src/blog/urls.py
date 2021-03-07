cf7b80de4c9ceea1cf7c479e62946666c79dddb8
Xarala <xaralaxarala@gmail.com>
2021-03-07 09:13:40 +0000

    Add sitemap

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,11 +1,21 @@
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path
+
+from blog.sitemaps import PostSitemap
 
 from . import views
 
 app_name = "blog"
+sitemaps = {"posts": PostSitemap}
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="posts"),
     path("<slug:slug>/", views.PostDetailView.as_view(), name="blog-detail"),
     path("tutorials/tags/<str:tag_name>/", views.blog_tag, name="blog_tag"),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
