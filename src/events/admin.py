18b80ba482a3f1db045ce574652e3e9ca0079652
Xarala <xaralaxarala@gmail.com>
2021-03-25 12:37:01 +0000

    Add events

--- src/events/admin.py
+++ src/events/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Event, Speaker
 
 
-admin.site.register(Event)
+class EventAdmin(SummernoteModelAdmin):
+    summernote_fields = ("content",)
+
+
+admin.site.register(Event, EventAdmin)
 admin.site.register(Speaker)
--- src/events/urls.py
+++ src/events/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "events"
+
 urlpatterns = [
     path("", views.EventListView.as_view(), name="events"),
     path("<slug:slug>/", views.EventDetailView.as_view(), name="event-detail"),
